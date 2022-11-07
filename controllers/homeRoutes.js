d0ac3401aa8c3d9461cceeae082d3680b05ef72d
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-11-07 19:59:58 +1100

    commit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,24 +1,20 @@
-const router = require('express').Router();
-const { } = require('../models');
+const router = require("express").Router();
+const {} = require("../models");
 // const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-    try {
-        res.render('homepage');
-    } catch (err) {
-        console.error(err);
-        res.status(500).json(err);
-
-    }
+router.get("/", async (req, res) => {
+  try {
+    res.render("homepage");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/signup', async (req, res) => {
-    try {
-        res.render('signup');
-    } catch (err) {
-        res.statusMessage(500).json(err);
-    }
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup");
+  } catch (err) {
+    res.statusMessage(500).json(err);
+  }
 });
-
-
-module.exports = router;
