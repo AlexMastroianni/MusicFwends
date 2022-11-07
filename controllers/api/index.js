a04e849ccc4944bc74162aaf2e45a9e6528b9bd8
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-11-07 19:27:15 +1100

    working on feedRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const feedRoutes = require('./feedRoutes');
 // const projectRoutes = require('./projectRoutes');
 
 router.use('/users', userRoutes);
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
-
-// const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const feedRoutes = require("./feedRoutes")
 
 router.use('/', homeRoutes);
 // router.use('/api', apiRoutes);
