baa9faea811dcffe73d200714e0b8e817c7efd94
Tobias Virgona <tobiasvirgona@gmail.com>
2022-11-07 22:16:21 +1100

    ran lint!

--- public/js/signup.js
+++ public/js/signup.js
@@ -35,40 +35,40 @@ let passwordInput = document.querySelector('#password-signup');
 const defaultOptions = [
   {
     id: 0,
-    value: "Too weak",
+    value: 'Too weak',
     minDiversity: 0,
     minLength: 0
   },
   {
     id: 1,
-    value: "Weak",
+    value: 'Weak',
     minDiversity: 2,
     minLength: 6
   },
   {
     id: 2,
-    value: "Medium",
+    value: 'Medium',
     minDiversity: 4,
     minLength: 8
   },
   {
     id: 3,
-    value: "Strong",
+    value: 'Strong',
     minDiversity: 4,
     minLength: 10
   }
 ];
 
-const passwordStrength = (password, options = defaultOptions, allowedSymbols = "!\"#\$%&'\(\)\*\+,-\./:;<=>\?@\[\\\\\\]\^_`\{|\}~") => {
+const passwordStrength = (password, options = defaultOptions, allowedSymbols = '!"#\$%&\'\(\)\*\+,-\./:;<=>\?@\[\\\\\\]\^_`\{|\}~') => {
 
   let passwordCopy = password || '';
 
   options[0].minDiversity = 0,
-    options[0].minLength = 0;
+  options[0].minLength = 0;
 
   const rules = [
     {
-      regex: "[a-z]",
+      regex: '[a-z]',
       message: 'lowercase'
     },
     {
