701ba9f06a8f3c7aa8f386b247a6c4d9a254ff1a
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-11-08 21:16:15 +1100

    from main

--- models/User.js
+++ models/User.js
@@ -38,17 +38,6 @@ User.init(
     profile_avatar: {
       type: DataTypes.STRING,
     },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    profile_avatar: {
-      type: DataTypes.STRING,
-    },
-    // posts: {
-    //   type: DataTypes.STRING('long'),
-    //   allowNull: true,
-    // },
   },
   {
     hooks: {
