c1c570e2eb2870e1cbf6ab22c556eaf921ce2b86
RJ Dust Puppies <109449181+RosemaryJF@users.noreply.github.com>
2022-11-12 12:30:06 +1100

    Changed user seed file to make emails and usernames more music related

--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -2,36 +2,36 @@ const userData = [
   {
     id: 1,
     email: 'feelincute@gmail.com',
-    password: 'secret1234',
-    username: 'poop',
+    password: 'secret12345',
+    username: 'in-a-barbie-world',
     profile_avatar: 'https://via.placeholder.com/150x150'
   },
   {
     id: 2,
-    email: 'feelincutey@gmail.com',
-    password: 'secret12345',
-    username: 'poopi',
+    email: 'styleziswild@gmail.com',
+    password: 'secret123456',
+    username: 'HarryStylezLover3',
     profile_avatar: 'https://via.placeholder.com/150x150'
   },
   {
     id: 3,
-    email: 'feelincuteyy@gmail.com',
-    password: 'secret123s',
-    username: 'poops',
+    email: 'linkspinksandstinks@gmail.com',
+    password: 'secret12345s',
+    username: 'SticksAndStones',
     profile_avatar: 'https://via.placeholder.com/150x150'
   },
   {
     id: 4,
-    email: 'feelincutessy@gmail.com',
-    password: 'secret123ss',
-    username: 'poopss',
+    email: 'realmusic@gmail.com',
+    password: 'secret12345ss',
+    username: 'real_music_is_made_outside_the_studio',
     profile_avatar: 'https://via.placeholder.com/150x150'
   },
   {
     id: 5,
-    email: 'feelincut@gmail.com',
-    password: 'secret12',
-    username: 'wee',
+    email: 'metallicarockson@gmail.com',
+    password: 'secret123456ss',
+    username: 'metallicaGuitarSolosRule',
     profile_avatar: 'https://via.placeholder.com/150x150'
   }
 ];
